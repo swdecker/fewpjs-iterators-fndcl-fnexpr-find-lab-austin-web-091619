bbda7c79e4305b708df0a98506e31e416e6d7d61
swdecker <swdecker@gmail.com>
2019-11-07 11:35:27 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(gameArray){
+  let winningGame = gameArray.find(gameObj => gameObj.result ==="W")
+  if (winningGame){
+    return winningGame.year
+  } else {
+    return undefined
+  }
+}
